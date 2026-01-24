fba892017775a2b07e15892d25b834f3ee4e76fa
Richard Fox <richard.fox@ahref.co.uk>
2026-01-23 18:58:00 -0800

    debugging for entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# Run the restore script
+echo "Starting entrypoint..."
 /usr/local/bin/restoreBackup.sh
+echo "restoreBackup.sh finished with exit code $?"
 
+echo "Executing CMD: $@"
 exec "$@"
