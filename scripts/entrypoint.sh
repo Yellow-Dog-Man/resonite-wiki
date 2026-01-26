aedcf5fe75d4235011331ce0eb5b59101f9044e9
Richard Fox <richard.fox@ahref.co.uk>
2026-01-25 19:31:20 -0800

    Check for status of restore and exit if fail
    Prevents container starting but not actually being successful

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,7 +2,13 @@
 
 echo "Starting entrypoint..."
 /usr/local/bin/restoreBackup.sh
-echo "restoreBackup.sh finished with exit code $?"
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    echo "restoreBackup.sh failed with exit code $exit_code"
+    exit $exit_code
+fi
+echo "restoreBackup.sh finished successfully."
+
 
 echo "Executing CMD: $@"
 exec "$@"
