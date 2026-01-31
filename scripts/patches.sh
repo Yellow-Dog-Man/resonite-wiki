336d4b077f2815789d75256b8c78ca1e8d9ba5d1
Richard Fox <richard.fox@ahref.co.uk>
2026-01-31 12:55:26 -0800

    Remove patches that are no longer required.
    These were only required when we were pulling backups from the old wiki as the migrations are now "done" and backed up on the wiki these patches will no longer be needed.

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -12,16 +12,7 @@ if [ -f extensions/UserFunctions/composer.json ]; then
     mv extensions/UserFunctions/composer.json extensions/UserFunctions/composer.bak
 fi
 
-# These next three commands, get us around some sort of migration that shouldn't happen but is happening.
-# I can remove this stuff, once we're not running the old wiki again, but until then let's deal with it.
-# Modify all patches to use IF EXISTS for DROP INDEX to avoid errors when indexes don't exist
-sed -i 's/DROP INDEX /DROP INDEX IF EXISTS /g' sql/mysql/*.sql
-
-# Comment out the ALTER TABLE in pagelinks patch to avoid corruption
-#sed -i 's/^ALTER TABLE /\-- ALTER TABLE /g' sql/mysql/patch-pagelinks-drop-pl_title.sql
-
-# Remove the ALTER TABLE blocks from categorylinks patch to avoid corruption
-#sed -i '/^ALTER TABLE/,/;/d' sql/mysql/patch-categorylinks-pk.sql
+# Any other manual commands to patch stuff here.
 
 # Mark that patches have been applied
 touch ./.patched
