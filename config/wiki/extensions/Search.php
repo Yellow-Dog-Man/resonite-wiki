a0b512947657fa4fdef05b453d59489581c2675e
ProbablePrime <prime@ahref.co.uk>
2026-06-26 21:11:50 -0700

    tweak(search): swap to the perfield_builder

--- config/wiki/extensions/Search.php
+++ config/wiki/extensions/Search.php
@@ -40,6 +40,26 @@ $wgCirrusSearchReplicas = [
 
 $wgCirrusSearchUseCompletionSuggester = 'yes';
 
+// Weights: These are the default settings, but presented here for commentary/docs reasons
+// $wgCirrusSearchWeights = [
+//     'title'          => 20,
+//     'redirect'       => 15,
+//     'category'       => 8,
+//     'heading'        => 5,
+//     'opening_text'   => 3,
+//     'text'           => 1,
+//     'auxiliary_text' => 0.5,
+//     'file_text'      => 0.5,
+// ];
+
+// The following settings are based on information from: https://gerrit.wikimedia.org/g/mediawiki/extensions/CirrusSearch/%2B/HEAD/docs/settings.txt
+// And Prime reading each of the files, his head hurt reading them :'(
+
+// This switches the query builder profile to the "perfield_builder" one, which can score and rank each field separately.
+// E.g. title gets its own score, text gets its own, before they were flattened together
+// See: /CirrusSearch/profiles/FullTextQueryBuilderProfiles.config.php for more info, but I barely understand it - PRIME
+$wgCirrusSearchFullTextQueryBuilderProfile = 'perfield_builder';
+
 # TMP
 #$wgDisableSearchUpdate = true;
 ?>
